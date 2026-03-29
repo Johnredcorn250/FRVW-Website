df325ddcd34c8a3cedcd8dc07c4f8a33abdb4cd8
Johnredcorn250 <166492095+Johnredcorn250@users.noreply.github.com>
2026-03-29 14:53:32 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// ⚠️ Change 'frvb-website' to your actual GitHub repository name
 export default defineConfig({
   plugins: [react()],
   base: '/FRVW-Website/',
